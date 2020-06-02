f45bf4110ff82e7b218fd9c378a1a54c7da1b336
GisangLee <masterkorea01@naver.com>
2020-06-02 23:24:38 +0900

    saving user data on local storage

---
+++ greeting.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentuser";
+const SHOWING_CN = "showing";
+
+function saveUser(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentuser = input.value;
+  saveUser(currentuser);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
